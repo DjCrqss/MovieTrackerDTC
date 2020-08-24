5a81f822124000813ddfd0367496cbe6685700f1
DJ Cross <dj3dxx@gmail.com>
2020-08-24 12:20:21 +1200

    3.2

--- GUI.java
+++ GUI.java
@@ -4,14 +4,14 @@ import java.io.*;
 import java.awt.Color;
 
 /**
- * Write a description of class GUI here.
+ * This class creates the main UI for the movie tracker
  * 
  * @author DJ
  * @version 2.1
  */
 public class GUI
 {
-    // instance variables - replace the example below with your own
+    // create MovieDatabase class as an object
     private MovieDatabase database1 = new MovieDatabase();
 
     /**
@@ -30,12 +30,14 @@ public class GUI
     {
         UI.clearGraphics();
         UI.println("\n------  Add Movie  ------");
+        // ask user for parameters of movie
         String title =  UI.askString("Movie title: ").trim();
         String director = UI.askString("Movie director: ").trim();
-        String genre = UI.askString("Movie genre: ").trim();int rating = 0;
+        String genre = UI.askString("Movie genre: ").trim();
+        int rating = 0;
         boolean askAgain = true;
         while (askAgain == true){
-            
+            // error checking for rating input belove 0 or above 5
             try{
                 rating = UI.askInt("Your rating (0-5): ");
                 if (rating < 0 || rating > 5){
@@ -48,7 +50,7 @@ public class GUI
                 UI.println("Your rating (0-5):  (must be between 0-5): ");
             }
         }
-        //String title1 = capitaliseSentence(title);
+        // add movie to HashMap as an object via method in MovieDatabase class
         database1.createMovie(capitaliseSentence(title), capitaliseSentence(genre), capitaliseSentence(director), rating);
         UI.println("Added " + title + " to the movie list.");
     }
@@ -90,7 +92,7 @@ public class GUI
     
     /**
      * Print info of movie
-     * 
+     * @param movieID   ID of the movie that is being searched
      * 
      */
     private void printInfo(int movieID)
@@ -107,27 +109,50 @@ public class GUI
             UI.print("☆");
         }
         UI.println();
-        // MAKE SURE TO DRAW A TITLE
-        // DRAW GUI
+        // Draw title for GUI
+        UI.setFontSize(32);
+        UI.drawString("Movie Information ", 50, 60);
+        // Draw GUI
         UI.setColor(Color.darkGray);
-        UI.fillRect(50, 50, 250, 250);
-        UI.drawImage("movie_icon.png", 75, 75, 200, 200);
+        UI.fillRect(50, 90, 250, 250);
+        UI.drawImage("movie_icon.png", 75, 115, 200, 200);
         UI.setFontSize(18);
         // Draw text
-        UI.drawString("Movie    | " + database1.returnMovieTitle(movieID), 50, 325);
-        UI.drawString("Director | " + database1.returnMovieDirector(movieID), 50, 350);
-        UI.drawString("Genre    | " + database1.returnMovieGenre(movieID), 48, 375);
-        UI.drawString("Rating   | ", 51, 400);
+        UI.drawString("Movie    | " + database1.returnMovieTitle(movieID), 50, 365);
+        UI.drawString("Director | " + database1.returnMovieDirector(movieID), 50, 390);
+        UI.drawString("Genre    | " + database1.returnMovieGenre(movieID), 48, 415);
+        UI.drawString("Rating   | ", 51, 440);
+        // Draw circles for star rating
         for (int i = 0; i < database1.returnMovieRating(movieID); i++){
-            UI.fillOval(130 + (22 * i), 385, 20, 20);
+            UI.fillOval(130 + (22 * i), 425, 20, 20);
         }
         for (int i = 5; i > database1.returnMovieRating(movieID); i--){
-            UI.drawOval(130 + (database1.returnMovieRating(movieID) * 22) + (22 * (5-i)), 385, 20, 20);
+            UI.drawOval(130 + (database1.returnMovieRating(movieID) * 22) + (22 * (5-i)), 425, 20, 20);
         }
+        // Find movie to recommend
+        //int topMovie = database1.returnTopMovieID(movieID);
+        
+            
+        String[] moviesList = database1.getTopRecommended(movieID);
+        if (moviesList[0] != "null"){
+            UI.drawString("Recommended:", 50, 475);
+        } else {
+            UI.drawString("No movie to recommend", 50, 475);
+        }
+        for (int i = 0; i < moviesList.length; i++){
+            if (moviesList[i] != "null"){
+                UI.setColor(Color.lightGray);
+                UI.fillRect(50, 485 + (55 * i), 250, 50);
+                UI.drawImage("movie_icon.png", 65, 495 + (i * 55), 32, 30);
+                UI.setColor(Color.darkGray);
+                UI.drawString(moviesList[i], 110, 517 + (i * 55));
+            }
+        }
+        
     }
     
     /**
-     * Display list of all movies
+     * Display list of all movies to GUI and print
      */
     private void displayMovies(){
         UI.clearGraphics();
@@ -136,14 +161,17 @@ public class GUI
         for (int i = 0; i < moviesList.length; i++){
              UI.println(moviesList[i]);
         }
-        // MAKE SURE TO DRAW A TITLE
+        // Draw title for GUI
+        UI.setFontSize(32);
+        UI.drawString("All Movies", 50, 60);
         UI.setFontSize(18);
-        // DRAW GUI
+        // Draw movies on screen
         for (int i = 0; i < moviesList.length; i++){
             UI.setColor(Color.lightGray);
-            UI.fillRect(50, 20 + (55 * i), 250, 50);
+            UI.fillRect(50, 80 + (55 * i), 250, 50);
+            UI.drawImage("movie_icon.png", 65, 90 + (i * 55), 32, 30);
             UI.setColor(Color.darkGray);
-            UI.drawString(moviesList[i], 75, 50 + (i * 55));
+            UI.drawString(moviesList[i], 110, 112 + (i * 55));
         }
     }
     
@@ -156,7 +184,7 @@ public class GUI
         GUI obj = new GUI();  // Make a GUI object
         
         UI.initialise();
-        UI.setWindowSize(720, 500);
+        UI.setWindowSize(720, 620);
         UI.addButton("Add Movie", obj::addMovie);
         UI.addButton("Search Movie", obj::searchMovie);
         UI.addButton("Display Movies", obj::displayMovies);
@@ -164,6 +192,9 @@ public class GUI
         UI.setDivider(0.5);     // must come after setting up buttons etc.
     }
  
+    /**
+     * Method to clear the GUI
+     */
     private void clearScreen(){
         UI.clearGraphics();
     }
--- MovieDatabase.java
+++ MovieDatabase.java
@@ -1,20 +1,18 @@
-
-
 /**
- * Store the Countries
+ * Store the Movies
  * 
  * @author DJ
  * @version 1
  */
 import java.util.HashMap;
+import java.util.Scanner;
+import java.io.*;
+import ecs100.*;
 
-public class MovieDatabase
+public class MovieDatabase 
 {
     // instance variables
-    //private Movie[] movieList = new Movie[100]; //stores 100 movies
     private HashMap<Integer, Movie> movieList;
-    //private HashMap<String, Country> countryInfections;
-    
     private int storedMovies = 0;
     
     /**
@@ -24,17 +22,55 @@ public class MovieDatabase
     {
         // initialise instance variables
         movieList = new HashMap<Integer, Movie>();
+        // open file
+        File myfile = new File("movies.txt");
+        try{
+            // create new movies.txt if it does not exist and then open with scanner
+            myfile.createNewFile();
+            Scanner scan = new Scanner(myfile);
+            // iterate through lines to add to add movie
+            int lineNumber = 0;
+            while(scan.hasNextLine()){
+                // read line and split data into pieces in array
+                String line = scan.nextLine();
+                String[] arr = line.split("(?=,)");
+                // call create movie function with data from array
+                this.createMovie(arr[0].substring(1), arr[1].substring(1), arr[2].substring(1), Integer.parseInt(arr[3].substring(1)));
+                lineNumber++;
+            }
+            scan.close();
+        } 
+        catch(IOException e){
+            return;
+        }
     }
     
+    /** 
+     * Saves movies to file
+     */
+    public void saveMovies(){
+        File myfile = new File("movies.txt");
+        try{
+            FileWriter myWriter = new FileWriter("movies.txt");
+            for (int i = 0; i < movieList.size(); i++){
+               myWriter.write("," + movieList.get(i).getMovieTitle() + "," + movieList.get(i).getDirector() + "," + movieList.get(i).getGenre() + "," + movieList.get(i).getRating() + String.format("%n"));
+            }
+            myWriter.close();
+        }
+        catch(IOException e){
+            return;
+        }
+    }
+    
+    /**
+     * Return all movies to GUI class
+     * @return movieArray   Array of movie titles
+     */
     public String[] getAllMovies(){
         String[] movieArray = new String[movieList.size()];
         for (int i = 0; i < movieList.size(); i++){
             movieArray[i] = movieList.get(i).getMovieTitle();
         }
-        // create array size of hashmap
-        // movieArray[count] = new(x,x,x,x);
-        // for loop in hashmap add objects to array
-        // return array to GUI which will have a loop for length to print data
         return movieArray;
     }
     
@@ -51,18 +87,16 @@ public class MovieDatabase
                         int rating)
     {
         movieList.put(storedMovies, new Movie(movieTitle, genre, director, rating));
-        /*movieList[storedMovies] = new Movie(movieTitle,
-                                            genre,
-                                            director,
-                                            rating);*/
         storedMovies++;
-        // return error if stored movie has same name
+        this.saveMovies();
+        // return error if stored movie has same name soon
     }
     
-    /** Get ID of Movie object based on title
-     * 
+    /** 
+     * Get ID of Movie object based on title
      */
     public int returnMovieID(String titleQuery){
+        // set returned movie num to -1 in case no movie is found
         int movieNum = -1;
         int resultsCount = 0;
         for (int i = 0; i < movieList.size(); i++){
@@ -72,14 +106,34 @@ public class MovieDatabase
             }
         }
         return movieNum;
-        //movieID = id in array
-        //return movieID;
-        
-        
     }
     
+    // make class that loops through all movies checking for highest rating, when finding a higher rating,
+    // check if the genre is the same or check if the director is the same
+    public String[] getTopRecommended(int movieID){
+        String[] movieArray = new String[2];
+        for (int i = 0; i < 2; i++){
+            movieArray[i] = "null";
+        }
+        int resultsCount = 0;
+        for (int i = 0; i < movieList.size(); i++){
+            if ( movieID != i && movieList.get(i).getRating() == 5){
+                if (resultsCount < 2){
+                    movieArray[resultsCount] = movieList.get(i).getMovieTitle();
+                    resultsCount++;
+                }
+            }
+        }
+        return movieArray;
+    }
+    
+    
+    
+    
+  
+    
     /**
-     * Get name of Movie object inside of movieList array
+     * Get name of Movie object inside of movieList HashMap
      * @param   int movieID  ID in array of selected movie
      * @return  String movieTitle
      */
@@ -89,18 +143,33 @@ public class MovieDatabase
         return movieTitle;
     }
     
+    /**
+     * Get director of Movie object inside of movieList HashMap
+     * @param   int movieID  ID in array of selected movie
+     * @return  String movieDirector
+     */
     public String returnMovieDirector(int movieID)
     {
         String movieDirector = movieList.get(movieID).getDirector();
         return movieDirector;
     }
     
+    /**
+     * Get genre of Movie object inside of movieList HashMap
+     * @param   int movieID  ID in array of selected movie
+     * @return  String movieGenre
+     */
     public String returnMovieGenre(int movieID)
     {
         String movieGenre = movieList.get(movieID).getGenre();
         return movieGenre;
     }
     
+    /**
+     * Get rating of Movie object inside of movieList HashMap
+     * @param   int movieID  ID in array of selected movie
+     * @return  String movieRating
+     */
     public int returnMovieRating(int movieID)
     {
         int movieRating = movieList.get(movieID).getRating();
